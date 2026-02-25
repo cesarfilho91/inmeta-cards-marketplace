4774792505edd7fbc4560c6ab21b18a1642d59c3
Cesar Filho <cesarfilho.info.apps@gmail.com>
2026-02-25 13:14:10 -0300

    feat: adicionar AppLayout como estrutura base da aplicação

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/auth.store'
+import { useRouter } from 'vue-router'
+
+const authStore = useAuthStore()
+const router = useRouter()
+
+const logout = () => {
+  authStore.logout()
+  router.push('/')
+}
+</script>
+
+<template>
+  <nav class="navbar">
+    <div class="logo">
+      TradeApp
+    </div>
+
+    <div class="actions">
+      <span v-if="authStore.user">
+        👤 {{ authStore.user.name }}
+      </span>
+
+      <button @click="logout">
+        Sair
+      </button>
+    </div>
+  </nav>
+</template>
