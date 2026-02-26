8d02cffd8d1e782235fe800e1412ef2ad7a1c03a
Cesar Filho <cesarfilho.info.apps@gmail.com>
2026-02-26 19:39:06 -0300

    refactor(app): centralizar inicialização no autenticação no router

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
+import { useAuthStore } from '@/stores/auth.store';
 import Toast from '@/components/Toast.vue'
+
+const auth = useAuthStore()
 </script>
 
 <template>
-  <RouterView />
-  <Toast />
+  <div v-if="auth.initialized">
+    <RouterView />
+    <Toast />
+  </div>
+
+  <div v-else class="app-loading">
+    Carregando...
+  </div>
 </template>
